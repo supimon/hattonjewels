373cf6f901e05c97bc28cf19ed748e32e605888a
supimon <supimon@gmail.com>
2016-06-24 14:05:46 +0530

    added engagement page

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,96 @@
 $(function(){
-    // make slides responsive
-    var sliderHeight;
-    setSlideHeight();
-    $(window).on('resize', setSlideHeight);
-    // utility function to set height of carousal
-    function setSlideHeight(){
+    // lookup table
+    var lookupTable = {
+                        sliderHeight: {xs:375, sm:375, md:463, lg:563},
+                        productSliderHeight: {xs:"auto", sm:"auto", md:"auto", lg:"auto"},
+                        sliderProdCount: {xs:2, sm:3, md:5, lg:5},
+                        productSliders: resetSliders(true),
+                    },
+        screenSize; // indicates current screenSize
+    makeResponsiveAdjustments();
+    $(window).on('resize', makeResponsiveAdjustments);
+    // make responsive adjustments to page elements
+    function makeResponsiveAdjustments(){
+        getscreenSize();
+        populateSliderProducts(null);
+    }
+    // utility function to get width of screen
+    function getscreenSize(){
         switch($('p.container.copyrights').outerWidth()){
-            case 1170: sliderHeight = 563; break;
-            case 970: sliderHeight = 463; break;
-            case 750: default: sliderHeight = 375; break;
+            case 1170: screenSize = "lg"; break;
+            case 970: screenSize = "md"; break;
+            case 750: screenSize = $("body").width() != 750 ? "sm": "xs"; break;
+            default: screenSize = "xs"; break;
         }
-        $('.slide-div').width($('.slider-section').width()).height(sliderHeight);
+    }
+    // make slides responsive
+    function adjustSliderHeight(){
+        $('.slider-section .slide-div').width($('.slider-section').width())
+            .height(lookupTable.sliderHeight[screenSize]);
+        $('.product-slider-section .slide-div').width($('.product-slider-section').width())
+            .height(lookupTable.productSliderHeight[screenSize]);
+    }
+    // populate slides
+    function populateSliderProducts(id){
+        if(id == null) {
+            if($('.products-slider-section .carousel-inner')) $('.products-slider-section .carousel-inner').empty();
+            lookupTable.productSliders = resetSliders(false);
+            $('.products-slider-section > div').each(function(i){
+                getSingleSliderProd($(this).attr('id'), true);
+            });
+        }
+        else getSingleSliderProd(id, false);
+    }
+    // utility function to initialise slider objects
+    function resetSliders(firstTime){
+        var tempObj = {};
+        $('.products-slider-section > div').each(function(i){
+            var that = this, thatID = $(that).attr('id');
+            tempObj[thatID] = {currentSlide:1 , path: $(that).attr('data-productPath'), dataExists:[false]};
+            firstTime ? $(this).on('slid.bs.carousel', function(){
+                lookupTable.productSliders[thatID].currentSlide = $('#'+thatID+' .item.active').index() + 1;
+                if(!lookupTable.productSliders[thatID].dataExists[lookupTable.productSliders[thatID].currentSlide])
+                    getSingleSliderProd(thatID, false);
+            }) : '';
+        });
+        return tempObj;
+    }
+    // utility function to get products for a single slider
+    function getSingleSliderProd(id, reloaded){
+        $.get(lookupTable.productSliders[id].path, function(data){
+            // on initial load prepare total slides
+            if(reloaded) {
+                var totalSlides = $(data).find('totalProducts').text()/lookupTable.sliderProdCount[screenSize],
+                    prodDiv = '';
+                for(var j = 0; j < lookupTable.sliderProdCount[screenSize]; j++){
+                    prodDiv += '<div class="col-md-2 col-sm-4 col-xs-6 text-center"><div class="product">'+
+                        '<h3 class="prod-title"></h3><p class="prod-price"></p>' +
+                        '</div></div>';
+                }
+                var active , holderDiv;
+                for(var j = 0; j < totalSlides; j++){
+                    active = j==0 ? 'active' : '';
+                    holderDiv = '<div class="item '+ active +'"><div class="slide-div"><div class="row">'+ prodDiv +
+                        '</div></div></div>';
+                    $('.carousel-inner').append(holderDiv);
+                }
+                // set height and instantiate carousel on the next tick due to race condition
+                setTimeout(function(){
+                    adjustSliderHeight();
+                    $('.carousel').carousel('pause');
+                }, 0);
+            }
+            // prepare individual slide
+            var prodArr = $(data).find('product'),
+                prodDivArr = $($('#'+id+' .item')[lookupTable.productSliders[id].currentSlide-1])
+                    .find('.product'),
+                firstSlide = (lookupTable.productSliders[id].currentSlide-1) * lookupTable.sliderProdCount[screenSize];
+            prodDivArr.each(function(i){
+                    $(this).css({'background-image':"url('"+ $(prodArr[firstSlide+i]).attr("imgURL") +"')"})
+                        .children(".prod-title").text($(prodArr[firstSlide+i]).text()).end()
+                        .children(".prod-price").text($(prodArr[firstSlide+i]).attr("price"));
+            });
+            lookupTable.productSliders[id].dataExists[lookupTable.productSliders[id].currentSlide] = true;
+        });
     }
 });
