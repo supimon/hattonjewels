cc1a79a53e27f375220e8e358cfe2aa43ec96d9a
supimon <supimon@gmail.com>
2016-07-20 01:13:30 +0530

    made the filters dynamic in brilliant cut page

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,132 @@
 $(function(){
     // lookup table
-    var lookupTable = {
+    var screenSize, // indicates current screenSize
+        filterSectionHeight = $('.filter-section').height(), // captures the expanded filter section height
+        minMaxFilterMarkup = '<div class="col-sm-6 col-md-4 single-slider-filter">' +
+            '<h3 class="filter-title"></h3>'+
+            '<input class="filter-holder" id="" type="text"/>'+
+            '<div class="twoTicks-slider-label-container">'+
+            '<div class="min-label"></div>'+
+            '<div class="max-label"></div>'+
+            '</div></div>',
+        fullValueFilterMarkup =  '<div class="col-sm-6 col-md-4 single-slider-filter">' +
+            '<h3 class="filter-title"></h3>'+
+            '<input class="filter-holder" id="" type="text"/>'+
+            '</div>',
+        labelFilterMarkup = '<div class="col-sm-6 col-md-4 single-slider-filter">'+
+            '<h3 class="filter-title"></h3>'+
+            '<div class="filter-holder label-filter" id="">'+
+            '<ul class="list-inline"></ul>'+
+            '</div></div>',
+        metalFilterMarkup = '<div class="col-sm-6 col-md-4 single-slider-filter">'+
+            '<h3 class="filter-title"></h3>'+
+            '<div class="filter-holder metal-filter" id="">'+
+            '<ul class="list-inline">'+
+            '</ul>'+
+            '</div></div>',
+        lookupTable = {
                         sliderHeight: {xs:375, sm:375, md:463, lg:563},
                         sliderProdCount: {xs:2, sm:3, md:5, lg:5},
                         filteredProdInsertAfterElement: {xs:1, sm:2, md:3, lg:4},
-                        productSliders: $('.products-slider-section').length ? resetSliders(true) : ''
-                    },
-        screenSize; // indicates current screenSize
+                        productSliders: $('.products-slider-section').length ? resetSliders(true) : '',
+                        filterMarkups: {
+                            "regular": fullValueFilterMarkup,
+                            "min-max": minMaxFilterMarkup,
+                            "radio-type": metalFilterMarkup,
+                            "label-type": labelFilterMarkup
+                        },
+                        filterSliders: {
+                            // slider name and id has to be unique
+                            cutSlider: {
+                                obj: {
+                                    id: "cutSlider",
+                                    min: 0, max: 10, range: true, value: [0, 2], step: 2,
+                                    tooltip: "hide",
+                                    ticks: [0, 2, 4, 6, 8],
+                                    ticks_labels: ["Excellent", "Very Good", "Good", "Fair"]
+                                },
+                                label: "Cut",
+                                type: "regular"
+                            },
+                            polishSlider:{
+                                obj: {
+                                    id: "polishSlider",
+                                    min: 0, max: 10, range: true, value: [0, 2], step: 2,
+                                    tooltip: "hide",
+                                    ticks: [0, 2, 4, 6, 8], ticks_labels: ["Excellent", "Very Good", "Good", "Fair"]
+                                },
+                                label: "Polish",
+                                type: "regular"
+                            },
+                            symmetrySlider:{
+                                obj: {
+                                    id: "symmetrySlider",
+                                    min: 0, max: 10, range: true, value: [0, 2], step: 2,
+                                    tooltip: "hide",
+                                    ticks: [0, 2, 4, 6, 8],
+                                    ticks_labels: ["Excellent", "Very Good", "Good", "Fair"]
+                                },
+                                label: "Symmetry",
+                                type: "regular"
+                            },
+                            caretSlider: {
+                                obj: {
+                                    id: "caretSlider",
+                                    min: 0.10, max: 30.00, range: true, value: [4.50, 20.50], step: 0.1,
+                                    tooltip: "always"
+                                },
+                                label: "Caret",
+                                type: "min-max"
+                            },
+                            claritySlider: {
+                                obj: {
+                                    id: "claritySlider",
+                                    min: 0, max: 10, range: true, value: [0, 2], step: 1,
+                                    tooltip: "hide",
+                                    ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                                    ticks_labels: ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
+                                },
+                                label: "Clarity",
+                                type: "regular"
+                            },
+                            colorSlider: {
+                                obj: {
+                                    id: "colorSlider", min: 0, max: 11, range: true, value: [2, 5], step: 1,
+                                    tooltip: "hide",
+                                    ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                    ticks_labels: ["IF", "VVS1", "VVS2", "VS1", "VS2", "SI1", "SI2", "SI3", "I1"]
+                                },
+                                label: "Color",
+                                type: "regular"
+                            },
+                            priceSlider: {
+                                obj: {
+                                    id: "priceSlider",
+                                    min: 200, max: 2500, range: true, value: [500, 1000], step: 1,
+                                    tooltip: "always"
+                                },
+                                label: "Price",
+                                type: "min-max"
+                            },
+                            metalFilter: {
+                                obj: {
+                                    id: "metalFilter",
+                                    values: ["Yellow", "Rose", "Platinum", "White"]
+                                },
+                                label: "Select a Metal",
+                                type: "radio-type"
+                            },
+                            certificateFilter: {
+                                obj: {
+                                    id: "certificateFilter",
+                                    values: ["Any", "GIA", "HRD", "IGI", "Other", "None"]
+                                },
+                                label: "Certificate",
+                                type: "label-type"
+                            }
+                        }
+                     };
+
     makeResponsiveAdjustments();
     $(window).on('resize', makeResponsiveAdjustments);
     // make responsive adjustments to page elements
@@ -93,21 +213,35 @@ $(function(){
         });
     }
     // sliders
-    if($('.filter-section').length) {
-        $("#cut-slider")
-            .slider({id: "cutSlider", min: 0, max: 10, range: true, value: [0, 2], tooltip: "hide"});
-        $("#polish-slider")
-            .slider({id: "polishSlider", min: 0, max: 10, range: true, value: [0, 2], tooltip: "hide"});
-        $("#symmetry-slider")
-            .slider({id: "symmetrySlider", min: 0, max: 10, range: true, value: [0, 2], tooltip: "hide"});
-        $("#caret-slider")
-            .slider({id: "caretSlider", min: 0.10, max: 30.00, range: true, value: [4.50, 20.50], tooltip: "always"});
-        $("#clarity-slider")
-            .slider({id: "claritySlider", min: 0, max: 10, range: true, value: [0, 2], tooltip: "hide"});
-        $("#color-slider")
-            .slider({id: "colorSlider", min: 0, max: 11, range: true, value: [2, 5], tooltip: "hide"});
-        $("#price-slider")
-            .slider({id: "priceSlider", min: 200, max: 2500, range: true, value: [500, 1000], tooltip: "always"});
+    for(sliderKey in lookupTable.filterSliders){
+        $($('.slider-filters .row')
+            .append(lookupTable.filterMarkups[lookupTable.filterSliders[sliderKey].type])
+            .children()[$('.slider-filters .row').children().length-1])
+            .children('.filter-title').text(lookupTable.filterSliders[sliderKey].label)
+            .end()
+            .children('.filter-holder').attr('id', lookupTable.filterSliders[sliderKey].obj.id+'Input');
+
+        switch(lookupTable.filterSliders[sliderKey].type){
+            case "radio-type":
+                lookupTable.filterSliders[sliderKey].obj.values.forEach(function(item, i){
+                    $('#'+lookupTable.filterSliders[sliderKey].obj.id+'Input .list-inline')
+                        .append('<li><span class="outer-circle '+ item.toString().toLowerCase() +'">' +
+                            '<span class="inner-circle '+ item.toString().toLowerCase() +'"></span></span><br/>' +
+                            item+'</li>');
+                });
+                break;
+            case "label-type":
+                lookupTable.filterSliders[sliderKey].obj.values.forEach(function(item, i){
+                    $('#'+lookupTable.filterSliders[sliderKey].obj.id+'Input .list-inline')
+                        .append('<li>'+item+'</li>');
+                });
+                break;
+        }
+    }
+    for(sliderKey in lookupTable.filterSliders){
+        if(lookupTable.filterSliders[sliderKey].type == "regular" ||
+            lookupTable.filterSliders[sliderKey].type == "min-max")
+        $('#'+lookupTable.filterSliders[sliderKey].obj.id+'Input').slider(lookupTable.filterSliders[sliderKey].obj);
     }
     // handle filtered product details view
     $('.filtered-product-section .product').parent().on('click', function(){
@@ -138,5 +272,19 @@ $(function(){
             $('.filtered-product-section .product.active').removeClass('active');
             $('.filtered-product-details').remove();
         });
+    });
+    // expand collapse filters
+    $('.collapse-expand .btn').on('click', function(){
+        var that = this;
+
+        $('.filter-section').hasClass('expanded') ?
+            $('.filter-section').animate({height: '0px'}, 500, function(){
+                $('.filter-section').removeClass('expanded');
+                $(that).children().removeClass('fa-angle-up').addClass('fa-angle-down');
+            }) :
+            $('.filter-section').animate({height: filterSectionHeight+'px'}, 500, function(){
+                $('.filter-section').addClass('expanded');
+                $(that).children().removeClass('fa-angle-down').addClass('fa-angle-up');
+            });
     });
 });
